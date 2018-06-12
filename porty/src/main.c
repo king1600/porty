9b7a79f2c3f6b86eaa0a4379982482b13b817711
king1600 <3dsarcard@gmail.com>
2018-06-11 21:07:29 -0500

    start on runtime vm

--- porty/src/main.c
+++ porty/src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "pt.h"
 
 int main(int argc, char* argv[]) {
+    pterm_t null = PT_NULL;
     printf("Nothing to see here atm\n");
     return 0;
 }
---
+++ porty/src/pt.h
@@ -0,0 +1,88 @@
+#ifndef _PT_H
+#define _PT_H
+
+#if defined(_WIN32) || defined(_WIN64)
+    #define PT_WINDOWS
+    #error "Windows is not currently supported"
+#elif defined(__linux__)
+    #define PT_LINUX
+#else
+    #error "Platform not supported"
+#endif
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef union { double f64; int32_t i32; uint64_t u64; uintptr_t ptr; } pterm_t;
+typedef struct pt_vm_t pt_vm_t;
+typedef struct pt_actor_t pt_actor_t;
+typedef struct { pterm_t head; pterm_t tail; } pt_list_t;
+typedef struct { uint32_t size; pterm_t* data; } pt_array_t;
+typedef struct { uint32_t size; uint8_t* data; } pt_binary_t;
+typedef struct { uint32_t idx; pterm_t key; pterm_t value; void* ptr; } pt_map_iter_t;
+typedef pterm_t (*pt_func) (pt_actor_t* env, int argc, pterm_t* argv);
+
+#define PT_TYPE_DATA   0
+#define PT_TYPE_PORT   1
+#define PT_TYPE_FUNC   2
+#define PT_TYPE_BINARY 3
+#define PT_TYPE_ARRAY  4
+#define PT_TYPE_LIST   5
+#define PT_TYPE_MAP    6
+#define PT_TYPE_ATOM   7
+#define PT_TYPE_INT    8
+#define PT_TYPE_FLOAT  9
+#define PT_NULL PT_FROM(PT_TYPE_ATOM, 0)
+#define PT_TRUE PT_FROM(PT_TYPE_ATOM, 1)
+#define PT_FALSE PT_FROM(PT_TYPE_ATOM, 2)
+
+#define PT_NAN_SHIFT 51
+#define PT_NAN_BITS 0b111111111111ULL
+#define PT_TERM_SIZE sizeof(pterm_t)
+#define PT_TERM_BITS (PT_TERM_SIZE << 3)
+#define PT_INT_BIT (1ULL << (PT_TERM_BITS - 1))
+#define PT_NAN_HEADER (PT_NAN_BITS << PT_NAN_SHIFT)
+#define PT_IS_NAN(v) (((v).u64 & PT_NAN_HEADER) == PT_NAN_HEADER)
+#define PT_FROM(type, value) ((pterm_t){ .u64 = (((PT_NAN_BITS << 3) | type) << 48) | value })
+#define PT_TYPE(v) (PT_IS_NAN(v) \
+    ? PT_TYPE_FLOAT : (((v).u64 & PT_INT_BIT) \
+    ? PT_TYPE_INT : (((v).u64 >> PT_NAN_SHIFT) & 0b111)))
+
+#define pt_is_float(v) (!PT_IS_NAN(v))
+#define pt_is_int(v) ((v).u64 & PT_INT_BIT)
+#define pt_is_data(v) (PT_TYPE(v) == PT_TYPE_DATA)
+#define pt_is_port(v) (PT_TYPE(v) == PT_TYPE_PORT)
+#define pt_is_func(v) (PT_TYPE(v) == PT_TYPE_FUNC)
+#define pt_is_binary(v) (PT_TYPE(v) == PT_TYPE_BINARY)
+#define pt_is_array(v) (PT_TYPE(v) == PT_TYPE_ARRAY)
+#define pt_is_list(v) (PT_TYPE(v) == PT_TYPE_LIST)
+#define pt_is_map(v) (PT_TYPE(v) == PT_TYPE_MAP)
+#define pt_is_atom(v) (PT_TYPE(v) == PT_TYPE_ATOM)
+
+pterm_t pt_alloc_data(size_t bytes);
+pterm_t pt_alloc_array(uint32_t items);
+pterm_t pt_alloc_map(bool is_probed);
+pterm_t pt_alloc_binary(uint32_t bytes);
+pterm_t pt_alloc_list(pterm_t head, pterm_t tail);
+inline pterm_t pt_alloc_float(double number) { pterm_t v; v.f64 = number; return v; }
+inline pterm_t pt_alloc_int(int32_t number) { pterm_t v = PT_FROM(PT_TYPE_INT,0); v.i32 = number; return v; }
+
+bool pt_get_list(pterm_t value, pt_list_t* list);
+bool pt_get_array(pterm_t value, pt_array_t* array);
+bool pt_get_binary(pterm_t value, pt_binary_t* binary);
+inline int32_t pt_get_int(pterm_t value) { return value.i32; }
+inline double pt_get_float(pterm_t value) { return value.f64; }
+void pt_map_iter_init(pterm_t map, pt_map_iter_t* it);
+bool pt_map_iter_has_next(pterm_t map, pt_map_iter_t* it);
+pterm_t pt_map_get(pterm_t map, uint64_t hash, pterm_t key);
+pterm_t pt_map_drop(pterm_t map, uint64_t hash, pterm_t key);
+pterm_t pt_map_put(pterm_t map, uint64_t hash, pterm_t key, pterm_t value);
+
+pterm_t pt_self();
+void pt_gcollect(bool major);
+uint64_t pt_hash(pterm_t value);
+bool pt_send(pterm_t port, pterm_t value);
+
+
+#endif // _PT_H
